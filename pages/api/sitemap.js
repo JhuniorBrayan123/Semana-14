4832379ebf4f263118b4c4b0e9e045306fb0f4d3
eddybrayam <kcbrayam@gmail.com>
2026-06-17 15:28:31 -0500

    Agregar ruta sitemap xml para Google

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
             .join("")}
   </urlset>`;
 
-    res.setHeader("Content-Type", "text/xml");
+    res.setHeader("Content-Type", "application/xml; charset=utf-8");
     res.write(sitemap);
     res.end();
 }
